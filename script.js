7b7f6afd850760588c9d1c3125ed93254f80151a
abdelrhman-mostafa95 <abdelrhman.mostafa1095@gmail.com>
2025-09-01 23:36:45 +0300

    finish changes

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ const elements = document.querySelectorAll('.fade-in');
   });
 
   elements.forEach(el => observer.observe(el));
+
+ const video = document.getElementById("video_speed");
+const soundBtn = document.getElementById("soundToggle");
+const soundIcon = document.getElementById("soundIcon");
+
+soundBtn.addEventListener("click", () => {
+  if (video.muted) {
+    video.muted = false;
+    soundIcon.classList.remove("bi-volume-mute");
+    soundIcon.classList.add("bi-volume-up");
+  } else {
+    video.muted = true;
+    soundIcon.classList.remove("bi-volume-up");
+    soundIcon.classList.add("bi-volume-mute");
+  }
+});
